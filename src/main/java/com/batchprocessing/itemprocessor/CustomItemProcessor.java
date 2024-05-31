6267ad6a6781208b75f11bdf01b8212589a6e95f
Deep-Inexture <deeppanchal@inexture.com>
2024-05-31 11:31:35 +0530

    Initial Commit

---
+++ src/main/java/com/batchprocessing/itemprocessor/CustomItemProcessor.java
@@ -0,0 +1,32 @@
+package com.batchprocessing.itemprocessor;
+
+import com.batchprocessing.entity.Customer;
+import lombok.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomItemProcessor implements ItemProcessor<Customer, Customer>{
+
+    private static final Logger log = LoggerFactory.getLogger(CustomItemProcessor.class);
+
+    @Override
+    public Customer process(@NonNull Customer item) {
+
+        if (!validateItem(item)) {
+            log.info("Customer with id {} is not valid", item.getId());
+            return null;
+        }else {
+            return item;
+        }
+    }
+
+    private boolean validateItem(Customer item) {
+        return item.getFirstName() != null && !item.getFirstName().isEmpty() &&
+               item.getLastName() != null && !item.getLastName().isEmpty() &&
+               item.getEmail() != null && !item.getEmail().isEmpty() &&
+               item.getProfession() != null && !item.getProfession().isEmpty();
+    }
+}
---
+++ src/main/java/com/batchprocessing/itemreader/CustomItemReader.java
@@ -0,0 +1,45 @@
+package com.batchprocessing.itemreader;
+
+import com.batchprocessing.entity.Customer;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomItemReader {
+
+    @Bean
+    public FlatFileItemReader<Customer> itemReader() {
+       FlatFileItemReader<Customer> reader = new FlatFileItemReader<>();
+       reader.setResource(new FileSystemResource("/home/root435/Downloads/spring-batch-processing-demo/src/main/resources/myrecords.csv"));
+       reader.setName("csv-reader");
+       reader.setLinesToSkip(1);   //skipping header line
+       reader.setLineMapper(lineMapper()); //take each line and convert that to java object
+       return reader;
+   }
+
+    private LineMapper<Customer> lineMapper() {
+        DefaultLineMapper<Customer> lineMapper = new DefaultLineMapper<>(); //to map lines from csv to Customer objects
+
+        //tokenizing data using comma as delimiters
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer(); //map tokenize field to the properties of customer class
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);  //if one column data value unavailable consider it as null
+        lineTokenizer.setNames("id", "firstname", "lastname", "email", "profession");
+
+        //to convert data to java object
+        BeanWrapperFieldSetMapper<Customer> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Customer.class);
+
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+
+        return lineMapper;
+    }
+
+}
