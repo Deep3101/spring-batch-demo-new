494af3b753f2193dccf0158c37b230130dcb282f
Deep-Inexture <deeppanchal@inexture.com>
2024-06-17 12:30:16 +0530

    Modified BatchConfig, CustomItemProcessor, CustomItemReader, CustomItemWriter

--- src/main/java/com/batchprocessing/itemreader/CustomerItemReader.java
+++ src/main/java/com/batchprocessing/itemreader/CustomerItemReader.java
@@ -8,32 +8,29 @@ import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CustomItemReader {
+public class CustomerItemReader {
 
     @Bean
-    public FlatFileItemReader<Customer> itemReader() {
+    public FlatFileItemReader<Customer> customerFlatFileItemReader() {
        FlatFileItemReader<Customer> reader = new FlatFileItemReader<>();
        reader.setResource(new ClassPathResource("myrecords.csv"));
        reader.setName("csv-reader");
-       reader.setLinesToSkip(1);   //skipping header line
-       reader.setLineMapper(lineMapper()); //take each line and convert that to java object
+       reader.setLinesToSkip(1);
+       reader.setLineMapper(customerLineMapper());
        return reader;
    }
 
-    private LineMapper<Customer> lineMapper() {
-        DefaultLineMapper<Customer> lineMapper = new DefaultLineMapper<>(); //to map lines from csv to Customer objects
+    private LineMapper<Customer> customerLineMapper() {
+        DefaultLineMapper<Customer> lineMapper = new DefaultLineMapper<>();
 
-        //tokenizing data using comma as delimiters
-        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer(); //map tokenize field to the properties of customer class
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
         lineTokenizer.setDelimiter(",");
-        lineTokenizer.setStrict(false);  //if one column data value unavailable consider it as null
+        lineTokenizer.setStrict(false);
         lineTokenizer.setNames("id", "firstname", "lastname", "email", "profession");
 
-        //to convert data to java object
         BeanWrapperFieldSetMapper<Customer> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
         fieldSetMapper.setTargetType(Customer.class);
 
--- src/main/java/com/batchprocessing/itemwriter/CustomerItemWriter.java
+++ src/main/java/com/batchprocessing/itemwriter/CustomerItemWriter.java
@@ -1,6 +1,7 @@
 package com.batchprocessing.itemwriter;
 
 import com.batchprocessing.entity.Customer;
+import lombok.NonNull;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,14 @@ import org.springframework.stereotype.Component;
 
 
 @Component
-public class CustomItemWriter implements ItemWriter<Customer> {
+public class CustomerItemWriter implements ItemWriter<Customer> {
+
+    private final JdbcTemplate jdbcTemplate;
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    public CustomerItemWriter(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     private boolean tableExists = false;
 
@@ -28,14 +33,19 @@ public class CustomItemWriter implements ItemWriter<Customer> {
     }
 
     @Override
-    public void write(Chunk<? extends Customer> chunk) throws Exception {
+    public void write(@NonNull Chunk<? extends Customer> chunk) {
         if (!tableExists) {
             createTable();
             tableExists = true;
         }
+        long startTime = System.nanoTime();
+
         for (Customer customer : chunk) {
             String sql = "INSERT INTO customer (id, first_name, last_name, email, profession) VALUES (?, ?, ?, ?, ?)";
             jdbcTemplate.update(sql, customer.getId(), customer.getFirstName(), customer.getLastName(), customer.getEmail(), customer.getProfession());
         }
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1_000_000; // Convert to milliseconds
+        System.out.println("Execution time for insert command: " + duration + " milliseconds");
     }
 }
